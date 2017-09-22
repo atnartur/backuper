274cf980bf9290ca6df8a9dbe9694e1edcc6c7fa
atnartur <i@atnartur.ru>
2017-09-22 21:32:11 +0300

    ! читаем только файлы

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 if __name__ == '__main__':
-    import os
+    from os import listdir, environ, path
 
-    tasks_dir_path = os.environ.get('TASKS_DIR', 'tasks')
+    tasks_dir_path = environ.get('TASKS_DIR', 'tasks')
 
     tasks_dir = []
     try:
-        tasks_dir = os.listdir(tasks_dir_path)
+        tasks_dir = listdir(tasks_dir_path)
     except FileNotFoundError:
         print('tasks dir not found')
         exit(1)
 
-    print(tasks_dir)
-
     for task_file in tasks_dir:
-        if task_file.endswith('.py') and not task_file.endswith('__init__.py'):
+        if path.isfile(path.join(tasks_dir_path, task_file)) and task_file.endswith('.py') and not task_file.endswith('__init__.py'):
             task_name = task_file[:-3]
             print('starting %s' % task_name)
             __import__(tasks_dir_path + '.' + task_name, globals(), locals())
