bfd55a773192cb3ba183eb093ee1778f03cd43dd
Artur <atnartur@users.noreply.github.com>
2017-09-23 20:11:49 +0300

    ! #7 temp adapter codestyle

--- adapters/temp.py
+++ adapters/temp.py
@@ -3,19 +3,17 @@ import shutil
 
 
 def getPath(projectName):
+    path = os.path.abspath('./' + projectName)
     try:
-        os.makedirs('./' + projectName, mode=0o777, exist_ok=False)
-        logging.info(u'Created folder')
-        return os.path.abspath('./' + projectName)
+        os.makedirs(path, mode=0o777, exist_ok=False)
     except FileExistsError:
-        logging.error(u'File exists error while creating folder')
-
+        pass
+    return path
 
 def clear(projectName):
     try:
         for file in os.listdir(path="./" + projectName):
             os.remove('./' + projectName + '/' + file)
         os.removedirs('./' + projectName)
-        logging.info(u'Folder deleted')
     except FileNotFoundError:
-        logging.error(u'File not found error while deleting folder')
+        logging.error(u'File not found error while deleting temp folder')
