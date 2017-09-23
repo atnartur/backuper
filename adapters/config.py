53a97c76e6b18ff1f99d75d7c99c11cea6191fd3
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 18:04:23 +0300

    Revert "+ #11 logging"
    This reverts commit 72e009e8b1ba6e0c9da67a9bc7edbfb3e53ec093.

--- adapters/config.py
+++ adapters/config.py
@@ -1,11 +1,7 @@
 import os
-import logging
 from yaml import load
 from app import tasks_dir_path
 
-logging.basicConfig(filename='../log/config.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
-
 
 def config(name):
-    logging.info(u'Opening config file')
     return load(open(os.path.join(tasks_dir_path, name + '.yml')))
