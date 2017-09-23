cf52b8dd99cc238bee3b9b3a9b902842967d08ee
Artur <atnartur@users.noreply.github.com>
2017-09-23 20:45:36 +0300

    - example ssh data

--- adapters/ssh.py
+++ adapters/ssh.py
@@ -1,12 +1,6 @@
 import paramiko
 import logging
 
-host = '192.168.0.8'
-user = 'login'
-secret = 'password'
-port = 22
-
-
 def ssh(host, user, password, port=22):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
