72e009e8b1ba6e0c9da67a9bc7edbfb3e53ec093
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 18:01:37 +0300

    + #11 logging
    + #11 Логирование

--- adapters/config.py
+++ adapters/config.py
@@ -1,7 +1,11 @@
 import os
+import logging
 from yaml import load
 from app import tasks_dir_path
 
+logging.basicConfig(filename='../log/config.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
+
 
 def config(name):
+    logging.info(u'Opening config file')
     return load(open(os.path.join(tasks_dir_path, name + '.yml')))
--- adapters/ssh.py
+++ adapters/ssh.py
@@ -1,14 +1,21 @@
 import paramiko
+import logging
 
 host = '192.168.0.8'
 user = 'login'
 secret = 'password'
 port = 22
 
+logging.basicConfig(filename='../log/ssh.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s : %(message)s')
+
+
 def ssh(host, user, password, port=22):
+    logging.info(u'SSHClient')
     client = paramiko.SSHClient()
+    logging.info(u'set_missing_host_key_policy')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # Подключение
+    logging.info(u'Connecting')
     client.connect(hostname=host, username=user, password=password, port=port)
     return client
 
