3da3b05030387ba44b276bdaf8440bb46890511f
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 22:29:49 +0300

    ! #7 #11 logging and folder adapter
    ! #7 added working with folder 'temp'
    ! #11 deleted logging

--- adapters/temp.py
+++ adapters/temp.py
@@ -1,23 +1,19 @@
 import os
-import logging
 import shutil
 
 
 def getPath(projectName):
-    path = os.path.abspath('./' + projectName)
+    path = os.path.abspath('../temp/' + projectName)
     try:
-        os.makedirs('./' + projectName, mode=0o777, exist_ok=False)
-        logging.info(u'Created folder')
+        os.makedirs('../temp/' + projectName, mode=0o777, exist_ok=False)
         return path
     except FileExistsError:
-        logging.error(u'File exists error while creating folder')
         pass
 
 
 def clear(projectName):
     try:
-        shutil.rmtree('./' + projectName)
-        logging.info(u'Folder deleted')
+        shutil.rmtree('../temp/' + projectName)
     except FileNotFoundError:
         pass
 
