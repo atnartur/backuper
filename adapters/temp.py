51cf77f187fa6ca5a89f1963e93f201c0cb8bdc9
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 20:02:45 +0300

    ! #7 folder adapter
    ! #7 Исправлена структура

--- adapters/temp.py
+++ adapters/temp.py
@@ -5,40 +5,20 @@ import shutil
 logging.basicConfig(level=logging.DEBUG, format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
 
 
-def create():
+def getPath(projectName):
     try:
-        os.makedirs('./temp', mode=0o777, exist_ok=False)
+        os.makedirs('./' + projectName, mode=0o777, exist_ok=False)
         logging.info(u'Created folder')
+        return os.path.abspath('./' + projectName)
     except FileExistsError:
         logging.error(u'File exists error while creating folder')
 
 
-def delete():
+def clear(projectName):
     try:
-        os.removedirs('./temp')
+        for file in os.listdir(path="./" + projectName):
+            os.remove('./' + projectName + '/' + file)
+        os.removedirs('./' + projectName)
         logging.info(u'Folder deleted')
     except FileNotFoundError:
         logging.error(u'File not found error while deleting folder')
-
-
-def listdir():
-    try:
-        temp = os.listdir(path="./temp")
-        logging.info(u'Listdir returned')
-        return temp
-    except FileNotFoundError:
-        logging.error(u'File not found error while listdir')
-
-
-def __add__(file):
-    try:
-        shutil.copy2(file, "./temp",  follow_symlinks=True)
-        logging.info(u'File: ' + file + ' added')
-    except FileNotFoundError:
-        logging.error(u'File not found error while adding file: ' + file)
-
-
-def clear():
-    for file in os.listdir(path="./temp"):
-        os.remove('./temp/' + file)
-    logging.info(u'Folder cleared')
