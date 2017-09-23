31b5c4f7615ffe30ef1337ee952a25f56a557670
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 22:54:29 +0300

    !  #7 folder adapter
    ! #7 fixed returning Null while folder is already exist
    ! #7 added joinig path

--- adapters/temp.py
+++ adapters/temp.py
@@ -3,18 +3,17 @@ import shutil
 
 
 def getPath(projectName):
-    path = os.path.abspath('../temp/' + projectName)
+    path = os.path.abspath(os.path.join('..', 'temp', projectName))
     try:
         os.makedirs(path, mode=0o777, exist_ok=False)
         return path
     except FileExistsError:
-        pass
+        return path
 
 
 def clear(projectName):
-    path = os.path.abspath('../temp/' + projectName)
+    path = os.path.abspath(os.path.join('..', 'temp', projectName))
     try:
         shutil.rmtree(path)
     except FileNotFoundError:
         pass
-
