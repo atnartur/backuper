3ccff2e0dc00d18d82cec942b21f1732dccb9e9e
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-23 22:37:46 +0300

    ! #11 logging
    ! #11 changed print to logging

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG, format= u'%(filename)s[LINE:%(lineno)d]
 try:
     config = load(open('config.yml'))
 except FileNotFoundError:
-    print('config not found')
+    logging.error(u'Config not found')
     exit(1)
 
 tasks_dir_path = environ.get('TASKS_DIR', 'tasks')
@@ -18,12 +18,12 @@ if __name__ == '__main__':
     try:
         tasks_dir = listdir(tasks_dir_path)
     except FileNotFoundError:
-        print('tasks dir not found')
+        logging.error(u'Task dir not found')
         exit(1)
 
     for task_file in tasks_dir:
         if path.isfile(path.join(tasks_dir_path, task_file)) and task_file.endswith('.py') and not task_file.endswith('__init__.py'):
             task_name = task_file[:-3]
-            print('starting %s' % task_name)
+            logging.error(u'starting %s' % task_name)
             task = __import__(tasks_dir_path + '.' + task_name, globals(), locals(), ['run'])
             task.run()
