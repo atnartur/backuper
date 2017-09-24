655ecf7e51e260cc741a15fcebabb7a26e70429f
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-24 23:18:55 +0300

    ! #8 #11 yandex adapter and logging
    ! #11 logging fixed (mb)
    ! #8 changed from call([]) to run()

--- adapters/uploader.py
+++ adapters/uploader.py
@@ -1,10 +1,9 @@
 import logging
 import os
-from subprocess import call
 from app import config
 
 
 def upload(local_path, project_name):
     for file in os.listdir(local_path):
         logging.info(u'Uploading ' + file)
-        logging.info(u'Result: ' + call(["YDCMD_TOKEN=" + config['yandex_disk']['token'] + " python ydcmd.py put " + os.path.join(local_path, file) + " " + os.path.join(config['yandex_disk']['dir'], project_name)]))
+        logging.info(u'Result: ' + os.subprocess.run("YDCMD_TOKEN=" + config['yandex_disk']['token'] + " python ydcmd.py put " + os.path.join(local_path, file) + " " + os.path.join(config['yandex_disk']['dir'], project_name), shell=True, check=True))
