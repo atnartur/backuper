b2ba62c0a9c04b4facfdc11784027fa55560d2e0
atnartur <i@atnartur.ru>
2017-09-24 22:57:51 +0300

    + config.yml -> env vars

--- adapters/uploader.py
+++ adapters/uploader.py
@@ -1,10 +1,11 @@
 import logging
 import os
 from subprocess import call
-from app import config
-
+from app import yadisk_dir, yadisk_token
 
 def upload(local_path, project_name):
     for file in os.listdir(local_path):
         logging.info(u'Uploading ' + file)
-        logging.info(u'Result: ' + call(["YDCMD_TOKEN=" + config['yandex_disk']['token'] + " python ydcmd.py put " + os.path.join(local_path, file) + " " + os.path.join(config['yandex_disk']['dir'], project_name)]))
+        src = os.path.join(local_path, file)
+        dest = os.path.join(yadisk_dir, project_name)
+        logging.info(u'Result: ' + call(["YDCMD_TOKEN=" + yadisk_token + " python ydcmd.py put " + src + " " + dest]))
