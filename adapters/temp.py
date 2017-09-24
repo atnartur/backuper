48ab0789385b5a85bcfbcda9a2796758910528e7
EvgeniyZhukovets <zhukovets199820@gmail.com>
2017-09-24 22:41:30 +0300

    ! folder adapter
    ! changed names

--- adapters/temp.py
+++ adapters/temp.py
@@ -2,16 +2,16 @@ import os
 import shutil
 
 
-def getPath(projectName):
-    path = os.path.abspath(os.path.join('..', 'temp', projectName))
+def get_path(project_name):
+    path = os.path.abspath(os.path.join('..', 'temp', project_name))
     try:
         return os.makedirs(path, mode=0o777, exist_ok=False)
     except FileExistsError:
         return path
 
 
-def clear(projectName):
-    path = os.path.abspath(os.path.join('..', 'temp', projectName))
+def clear(project_name):
+    path = os.path.abspath(os.path.join('..', 'temp', project_name))
     try:
         shutil.rmtree(path)
     except FileNotFoundError:
