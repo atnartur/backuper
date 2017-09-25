5862e0d5db9ad0b3b19c094e45276fbab636f394
atnartur <i@atnartur.ru>
2017-09-25 11:16:29 +0300

    ! #8 загрузка в правильную папку

--- adapters/uploader.py
+++ adapters/uploader.py
@@ -7,7 +7,7 @@ def upload(local_path, project_name):
     for file in os.listdir(local_path):
         logging.info(u'Uploading ' + file)
         src = os.path.join(local_path, file)
-        dest = os.path.join(yadisk_dir, project_name)
+        dest = os.path.join(yadisk_dir, project_name + '/')
         print(dest)
         try:
             subprocess.run('YDCMD_TOKEN=%s ydcmd put %s %s --verbose' % (yadisk_token, src, dest), shell=True, check=True)
