b59d200fc3dd7b41ccf1a297689582dd6bbf4f2e
atnartur <i@atnartur.ru>
2018-01-23 16:19:22 +0300

    + выкидывание исключения при невозможности загрузки

--- adapters/uploader.py
+++ adapters/uploader.py
@@ -16,6 +16,8 @@ def upload(local_path, project_name):
             my_env["YDCMD_TOKEN"] = yadisk_token
             proc = subprocess.Popen('ydcmd put %s %s --verbose' % (src, dest), shell=True, env=my_env)
             outs, errs = proc.communicate()
+            if errs:
+                raise ConnectionError(errs)
         except subprocess.CalledProcessError as e:
             logging.error('ydcmd error %s %s' % (e.stdout, e.stderr))
 
