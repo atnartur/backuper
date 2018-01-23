1caac23596489aca46205431b4f3997590707312
atnartur <i@atnartur.ru>
2018-01-23 13:36:03 +0300

    + сообщения о начале и завершении работы

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from os import listdir, environ, path
 import logging
-
+import time
 
 logging.basicConfig(level=logging.INFO, format= u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
 
@@ -16,10 +16,16 @@ if __name__ == '__main__':
         logging.error(u'Task dir not found')
         exit(1)
 
+    print('starting')
+    start_time = time.time()
+    tasks_count = 0
+
     for task_file in tasks_dir:
         if path.isfile(path.join(tasks_dir_path, task_file)) and task_file.endswith('.py') and not task_file.endswith('__init__.py'):
             task_name = task_file[:-3]
             logging.info(u'starting %s' % task_name)
             task = __import__(tasks_dir_path + '.' + task_name, globals(), locals(), ['run'])
             task.run()
+            tasks_count += 1
 
+    print('finished: %s tasks processed on %.2f seconds' % (tasks_count, time.time() - start_time))
