285b58feaf2a9326f0021d6836e2ecfad370835e
atnartur <i@atnartur.ru>
2018-01-23 13:16:16 +0300

    + #13 Загружать в Яндекс.Диск в подпапку по дате

--- adapters/uploader.py
+++ adapters/uploader.py
@@ -1,13 +1,15 @@
 import logging
 import os
 import subprocess
+import datetime
 from app import yadisk_dir, yadisk_token
 
 def upload(local_path, project_name):
     for file in os.listdir(local_path):
         logging.info(u'Uploading ' + file)
         src = os.path.join(local_path, file)
-        dest = os.path.join(yadisk_dir, project_name + '/')
+        today = datetime.date.today()
+        dest = os.path.join(yadisk_dir, project_name + '/', '%s-%s-%s/' % (today.year, today.month, today.day))
 
         try:
             subprocess.run('YDCMD_TOKEN=%s ydcmd put %s %s --verbose' % (yadisk_token, src, dest), shell=True, check=True)
